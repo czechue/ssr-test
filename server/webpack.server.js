6f586212ebd205492b4c337a101ff647fbd177e7
czechue <michal.lester87@gmail.com>
2018-02-25 20:27:49 +0100

    finished Section 2

---
+++ server/webpack.server.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+module.exports = {
+	// Inform webpack that were building a bundle for nodeJS,
+	// rather than for the browser
+	target: 'node',
+
+	// tell webpack the root file of server app
+	entry: './src/index.js',
+
+	// tell webpack where to put output file
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'build')
+	},
+
+	// tell webpack to run babel on every file it runs through
+	module: {
+		rules: [
+			{
+				test: /\.js?$/,
+				loader: 'babel-loader',
+				exclude: /node_modules/,
+				options: {
+					presets: [
+						'react',
+						'stage-0',
+						[ 'env', { targets: { browsers: [ 'last 2 versions' ] } } ]
+					]
+				}
+			}
+		]
+	}
+};
